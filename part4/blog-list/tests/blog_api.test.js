53bf2f9506051888a5e1679b6fcb986cfd6117f2
heyDante <badalsaibo@gmail.com>
2020-02-12 19:18:22 +0530

    4.9*: Blog list tests, step2

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -45,6 +45,11 @@ test('the amount of blogs is as expected', async () => {
   expect(response.body.length).toBe(initialBlogs.length);
 });
 
+test('the UID is named \'id\'', async () => {
+  const response = await api.get('/api/blogs');
+  expect(response.body[0].id).toBeDefined();
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
