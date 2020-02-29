2c1238dbeb4929ee06d8e7a933cab582fd7db935
heyDante <badalsaibo@gmail.com>
2020-02-29 22:29:21 +0530

    4.17: bloglist expansion, step6

--- part4/blog-list/controllers/blogsRouter.js
+++ part4/blog-list/controllers/blogsRouter.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 const blogsRouter = express.Router();
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1 });
   res.json(blogs.map((blog) => blog.toJSON()));
 });
 
 
 /* -- POST method -- */
 blogsRouter.post('/', async (req, res, next) => {
-
-  const blog = new Blog(req.body); // create a new object from the model
-  
   try {
+    const body = req.body;
+    const userId = body.userId;
+    const user = await User.findById(userId);
+
+    const blog = new Blog({ // create a new object from the model
+      title: body.title,
+      author: body.author || '',
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    });
+
     const savedBlog = await blog.save();
+
+    /* -- Saving the saved blogs' id to the user,
+    It's an array of object so we are concating -- */
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     res.status(201).json(savedBlog); 
   } catch (error) {
     next(error);
--- part4/blog-list/controllers/usersRouter.js
+++ part4/blog-list/controllers/usersRouter.js
@@ -4,7 +4,8 @@ const brcypt = require('bcrypt');
 const User = require('../models/user');
 
 usersRouter.get('/', async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find({})
+    .populate('blogs', { url: 1, title: 1, author: 1 });
   res.send(users);
 });
 
