0c56a1f3623a477e090e52a99a4214552c0990a7
heyDante <badalsaibo@gmail.com>
2020-03-03 23:23:42 +0530

    5.1: bloglist frontend, step1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
+import Blog from './components/Blog';
+
+import loginService from './services/login';
+import blogService from './services/blogs';
+
+import './App.css';
 
 function App() {
+  const [ blogs, setBlogs ] = useState([]);
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ user, setUser ] = useState(null);
+
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    isInitialMount.current ? isInitialMount.current = false :
+    (async () => {
+      const blogsFetched = await blogService.getAll();
+      console.log(blogsFetched, user);
+      const filteredBlogs = blogsFetched.filter((blog) => blog.user.username === user.username);
+      setBlogs(filteredBlogs);
+    })();
+  }, [user]);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const userDetails = await loginService.login({ username, password });
+      setUser(userDetails);
+    } catch (error) {
+      console.log('Invalid username or password');
+    }
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log In</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label htmlFor='username'>username</label>
+            <input id='username' type='text' value={username} onChange={({ target }) => setUsername(target.value)}/>
+          </div>
+          
+          <div>
+            <label htmlFor='password'>password</label>
+            <input id='password' type='password' value={password} onChange={({ target }) => setPassword(target.value)}/>
+          </div>
+  
+          <button type='submit'>Log In</button>
+        </form>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <p>{`${user.name} logged in`}</p>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
     </div>
   );
 }
