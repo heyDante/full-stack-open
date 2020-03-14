f32b573c1c9e8965e90e8785dc382705ed9bbfd3
heyDante <badalsaibo@gmail.com>
2020-03-14 21:44:14 +0530

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog/Blog.js
+++ part5/bloglist-frontend/src/components/Blog/Blog.js
@@ -6,20 +6,19 @@ const Blog = ({ blog }) => {
 
   const [ showMore, setShowMore ] = useState(false);
 
-  console.log(blog);
-
   return (
     <div className='blog'>
-      <h2 
-        className='blog-title'
-        onClick={() => setShowMore(!showMore)}
-      >
+      <h2 className='blog-title'>
         {blog.title}
+        <span>
+          <button onClick={() => setShowMore(!showMore)}>{showMore ? 'hide' : 'view'}</button>
+        </span>
       </h2>
       <span style={{fontStyle: 'italic'}}>by {blog.author}</span>
       <div className={ showMore ? 'blog-details' : 'hidden'}>
-        <a href={blog.url}>{blog.url}</a>
         <div>{blog.likes} likes  <button>like</button></div>
+        <a href={blog.url}>{blog.url}</a>
+        <p>{blog.user.name}</p>
       </div>
     </div>
   );
